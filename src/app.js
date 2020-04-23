e4b0f9d4c14242db6ca1759385566fcca76d3e31
Dyego Soriano <dyegodf2@gmail.com>
2020-04-23 20:19:54 -0300

    refatorando upload de arquivos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import { resolve } from 'path'
+
 import routes from './routes'
 
 // importando conexão com banco de dados
@@ -14,6 +16,7 @@ class App {
 
   middlewares () {
     this.server.use(express.json())
+    this.server.use('/files', express.static(resolve(__dirname, '..', 'tmp', 'uploads')))
   }
 
   routes () {
