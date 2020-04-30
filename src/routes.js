83680d80a85fc793532066c626f99293797c3749
Dyego Soriano <dyegodf2@gmail.com>
2020-04-30 17:16:49 -0300

    tornando as rotas mais legiveis

--- src/routes.js
+++ src/routes.js
@@ -12,17 +12,19 @@ import authMiddleware from './app/middleware/auth'
 const routes = new Router()
 const upload = multer(multerConfig)
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
-
-routes.use(authMiddleware)
-
-routes.put('/users', UserController.update)
-
-routes.get('/users/meetups', MeetupController.index)
-routes.post('/users/meetups', MeetupController.store)
-routes.put('/users/meetups/:meetup_id', MeetupController.update)
-
-routes.post('/files', upload.single('file'), FileController.store)
+routes
+  .post('/users', UserController.store)
+  .post('/sessions', SessionController.store)
+  // Autenticação JWT
+  .use(authMiddleware)
+  // Users
+  .put('/users', UserController.update)
+  // Meetups
+  .get('/meetups', MeetupController.index)
+  .post('/meetups', MeetupController.store)
+  .put('/meetups/:id', MeetupController.update)
+  .delete('/meetups/:id', MeetupController.delete)
+  // Files
+  .post('/files', upload.single('file'), FileController.store)
 
 export default routes
