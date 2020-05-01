d8652f7537962d81ceda6444d6185317018189ea
Dyego Soriano <dyegodf2@gmail.com>
2020-05-01 13:07:49 -0300

    melhorando erros

--- src/server.js
+++ src/server.js
@@ -1,3 +1,16 @@
 import app from './app'
 
+// notFound
+app.use((request, response, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+// Catch all
+app.use((error, request, response, next) => {
+  response.status(error.status || 500)
+  response.json({ error: error.message })
+})
+
 app.listen(3333)
