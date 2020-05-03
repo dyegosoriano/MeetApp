c6ad794af15912c0251c6277a44eace31dad704e
Dyego Soriano <dyegodf2@gmail.com>
2020-05-03 20:11:02 -0300

    inscrição de meetup

---
+++ src/app/controllers/SubscriptionController.js
@@ -0,0 +1,82 @@
+import { Op } from 'sequelize'
+import { startOfHour, addHours } from 'date-fns'
+
+import Meetup from '../models/Meetup'
+import File from '../models/File'
+import User from '../models/User'
+
+class SubscriptionController {
+  async store (request, response, next) {
+    try {
+      const { userId } = request
+
+      const meetup = await Meetup.findOne({
+        where: { id: request.params.id },
+        include: [
+          {
+            model: File,
+            as: 'banner',
+            attributes: ['name', 'url']
+          },
+          {
+            model: User,
+            as: 'owner',
+            attributes: ['name', 'email']
+          }
+        ]
+      })
+
+      // Verificando existencia de meetup
+      if (!meetup) {
+        return response
+          .status(400)
+          .json({ error: 'Meetup does not exists' })
+      }
+
+      // Veridicando se meetup ainda esta disponível
+      if (meetup.date < new Date()) {
+        return response
+          .status(400)
+          .json({ erro: 'Meetup is already finished' })
+      }
+
+      // Verificando se o usuário e dono do meetup
+      if (meetup.user_id === userId) {
+        return response
+          .status(406)
+          .json({ error: "The meetup owner can't subscribe" })
+      }
+
+      // Validando se suário ja esta inscrito
+      if (meetup.subscribers.includes(userId)) {
+        return response
+          .status(400)
+          .json({ error: 'Already subscribed' })
+      }
+
+      // Verificando se existe conflito de horários
+      const hourStart = startOfHour(Number(meetup.date))
+      const conflictMeetups = await Meetup.findOne({
+        where: {
+          subscribers: { [Op.contains]: [userId] },
+          date: { [Op.between]: [hourStart, addHours(hourStart, 2)] }
+        },
+        attributes: ['id', 'title', 'adress', 'date']
+      })
+
+      if (conflictMeetups) {
+        return response
+          .status(400)
+          .json({ error: 'You are already subscribed to a meetapp at the same time' })
+      }
+
+      await meetup.update({ subscribers: [userId, ...meetup.subscribers] })
+
+      return response.json(meetup)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export default new SubscriptionController()
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import FileController from './app/controllers/FileController'
 import MeetupController from './app/controllers/MeetupController'
+import SubscriptionController from './app/controllers/SubscriptionController'
 
 import authMiddleware from './app/middleware/auth'
 
@@ -28,6 +29,9 @@ routes
   .post('/meetups', MeetupController.store)
   .put('/meetups/:id', MeetupController.update)
   .delete('/meetups/:id', MeetupController.delete)
+
+// Subscriptions
+  .post('/subscriptions/:id', SubscriptionController.store)
 
   // Files
   .post('/files', upload.single('file'), FileController.store)
